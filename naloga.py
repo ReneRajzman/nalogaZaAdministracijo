cd3983ed5a3182b3221eaa87bb1f40b33919c772
ReneRajzman <rene.rajzman@student.um.si>
2023-03-23 21:24:48 +0100

    commiting basic task template

---
+++ naloga.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+def my_roberts(slika):
+    ##
+
+    return slika_robov
+
+
+def my_prewitt(slika):
+    ##
+    return slika_prewitt
+
+
+
+def my_sobel(slika):
+    ##
+
+    return slika_sobel
+
+
+
+def my_canny(slika, sp_prag, zg_prag):
+    ##
+    return slika_robov
+
+
+def spremeni_kontrast(slika, alfa, beta):
+    ##
+    return nova_slika
+
+
+def main():
+    ##
+
+if __name__ == '__main__':
+    main()
